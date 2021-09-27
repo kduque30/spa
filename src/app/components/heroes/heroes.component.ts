9c73282bf3126ff4b0ca410808615a401af2b1fe
kduque30 <khevinsdu18@hotmail.com>
2021-09-27 02:01:28 -0500

    avance de router

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService, Heroe} from '../../servicios/heroes.service'
+import { Router } from '@angular/router';
+import { HeroesService, Heroe} from '../../servicios/heroes.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html'
@@ -8,10 +9,15 @@ export class HeroesComponent implements OnInit {
 
   heroes:Heroe[] = [];
 
-  constructor( private _heroesService: HeroesService) { }
+  constructor( private _heroesService: HeroesService,
+                private router: Router) { }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx: number){
+    this.router.navigate(['heroe',idx])
   }
 
 }
